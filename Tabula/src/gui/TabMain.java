1277d1957fe4fa3cd5eb1c469c6d7e68f02ca087
mario <mario.saccone@gmail.com>
2016-05-31 00:19:49 +0200

    Inserted TabMain class

---
+++ Tabula/src/gui/TabMain.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class TabMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tabula launched");
+		//new TabGui();
+	}
+
+}
